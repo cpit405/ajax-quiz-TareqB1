f324cf8896210c7e455486c9e3161803c7180727
TareqB <128943091+TareqB1@users.noreply.github.com>
2023-11-13 07:57:20 +0000

    Update package-lock.json and script.js

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ getReposButton.addEventListener('click', async () => {
     const url = `https://api.github.com/users/${username}/repos`;
     const ulElement = document.getElementById('repos-list');
     // Fetch and display the list of repositories as li elements inside the ul element.
+    const response = await fetch(url);
+    const data = await response.json();
+
+    
+    data.forEach((item) => {
+        const liElement = document.createElement('li');
+        let a = document.createElement("a")
+        a.href= item.html_url
+        a.innerText = item.full_name
+        liElement.innerText = item.full_name;
+        liElement.appendChild(a);
+        ulElement.appendChild(liElement);
+    });
 
 });
 
